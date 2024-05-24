35f51f5d813b97a2634369a9e0197b8c67728724
anullator <danyelle.joy@gmail.com>
2024-05-23 20:20:49 -0500

    fixed inquirer bugs

--- db/schema.sql
+++ db/schema.sql
@@ -20,7 +20,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL, -- to hold employee first name
     last_name VARCHAR(30) NOT NULL, -- to hold employee last name
     role_id INTEGER NOT NULL, -- to hold reference to employee role
-    manager_id INTEGER, -- to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
+    manager_id INTEGER -- to hold reference to another employee that is the manager of the current employee (null if the employee has no manager)
 );
 
 
